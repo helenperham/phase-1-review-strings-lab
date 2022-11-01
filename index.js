3b68f9184a0676249ecfb48ebff3770e76a6a602
Helen <helenmperham@gmail.com>
2022-11-01 09:33:45 -0400

    commit tree 74aa5f8528636707f9e1d909274c96b621cce38d

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+
+const currentUser = "Fiona Shrecke";
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!` ;
